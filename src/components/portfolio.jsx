5c38b0d3756be34e2b7c8df068df2c1fa8c9aa21
dwik007 <dwinurfirmanto@gmail.com>
2024-01-31 18:30:34 +0700

    update

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -3,7 +3,51 @@ import React from "react"
 export default function Portfolio() {
   return (
     <div id="portfolio" className="section">
-      Portfolio
+      <div className="container mx-auto max-w-[1200px] px-3 pt-3 pb-20">
+        <div className="flex flex-col lg:align-center lg:flex-row justify-between mb-[50px]">
+          <div className="order-2 lg:order-1 mt-5">
+            <h4 className="text-secondary font-secondary text-[24px] ">
+              Recent Work
+            </h4>
+            <div className="text-gradient font-primary max-w-[700px] mx-auto text-[24px]">
+              Excited to showcase my recent work? Enclosed is a portfolio
+              featuring websites I've crafted. I hope this provides a better
+              glimpse into my capabilities and design style.
+            </div>
+          </div>
+          <div className="order-1 lg:order-2 text-center">
+            <h2 className="text-gradient font-primary max-w-[700px] mx-auto text-[50px] mt-12">
+              01
+            </h2>
+            <div>Completed Projects</div>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 text-center">
+          <div className="w-full">
+            <a href="https://www.hashmicro.com/id/iklan/software-erp">
+              <div className="bg-white h-full w-full justify-center items-center flex rounded-lg overflow-hidden p-5 relative group">
+                <img src="images/hashmicro-web.png" alt="" />
+                <div className="absolute p-5 bg-primary w-full h-full flex item opacity-0 group-hover:opacity-100 transition">
+                  <div className="text-center w-full text-white mt-[-60px] group-hover:mt-[0] transition-all">
+                    <div className="bg-white rounded-full px-5 inline-blick mb-2">
+                      <h4 className="text-gradient">Hashmicro Indonesia</h4>
+                    </div>
+                    <p className="text-[12px]">
+                      Crafting a dynamic landing page for HashMicro Indonesia
+                      involves utilizing OwlCarousel, JavaScript, HTML5, and
+                      CSS3. OwlCarousel offers interactive content displays,
+                      while JavaScript adds dynamic elements for a responsive
+                      interface. HTML5 ensures structural integrity, and CSS3
+                      polishes the design, resulting in an engaging landing page
+                      experience.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -5,17 +5,19 @@ export default function Profile() {
     <div id="profile" className="section">
       <div className="container mx-auto max-w-[1200px] p-3 overflow-hidden">
         <div className="font-secondary text-center font-bold mb-12">
-          <h4 className="text-secondary mb-3 ">Awesome Skill</h4>
-          <h2 className="text-gradient font-primary max-w-[700px] mx-auto text-[24px]">
-            I am a web developer who loves all things about technology. The
-            technologies that I master as a web developer include HTML, CSS,
-            JavaScript, PHP, MySQL, MongoDB, which is also supported by my
-            ability to solve problems effectively.
+          <h4 className="text-secondary mb-3 text-[30px]">Awesome Skill</h4>
+          <h2 className="text-gradient font-primary max-w-[1100px] mx-auto text-[24px]">
+            My expertise lies in the realm of front-end development, where I
+            excel in creating engaging and user-friendly web interfaces. With a
+            robust skill set in essential front-end technologies, I specialize
+            in building responsive and visually appealing websites. My focus on
+            creating seamless user experiences across various platforms and
+            devices allows me to deliver high-quality, adaptable web solutions.
           </h2>
         </div>
         <div className="flex flex-col lg:flex-row items-start justify-between">
           <div className="text-center w-full max-w-[450px] mx-auto mb-[50px]">
-            <div className="min-w-[300] min-h-[300px] bg-gradient pt-[50px] rounded-full overflow-hidden">
+            <div className="min-w-[300] min-h-[300px] bg-gradient mt-[4.5rem] pt-[50px] rounded-full overflow-hidden">
               <img src="/images/Profildwi.jpg" alt="" />
             </div>
             <h2 className="text-gradient text-[48px] mt-[20px]">
@@ -23,18 +25,18 @@ export default function Profile() {
             </h2>
           </div>
           <div className="w-full lg:pt-[50px] lg:ml-12">
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 text-center">
+            <div className="grid grid-cols-2 lg:grid-cols-2 gap-4 text-center">
               <div className="w-full">
                 <div className="bg-white h-full justify-center items-center flex rounded-lg overflow-hidden relative p-5 group">
-                  <img src="/images/1.png" alt="" />
+                  <img src="/images/js-logo.png" alt="" />
                   <div className="absolute p-5 backdrop-blur-xl bg-primary w-full h-full flex items-center opacity-0 group-hover:scale-100 group-hover:opacity-100 transition ease-linear">
                     <div className="text-center w-full text-white">
                       <div className="bg-white rounded-full px-5 py-2 inline-block mb-2">
-                        <h4 className="text-gradient">jquery</h4>
+                        <h4 className="text-gradient">JavaScript</h4>
                       </div>
                       <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolorem illo blanditiis{" "}
+                        Versatile scripting language simplifying HTML element
+                        manipulation and enabling interactive web development.{" "}
                       </p>
                     </div>
                   </div>
@@ -49,8 +51,8 @@ export default function Profile() {
                         <h4 className="text-gradient">Bootstrap</h4>
                       </div>
                       <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolorem illo blanditiis{" "}
+                        Front-end framework facilitating responsive website
+                        development with pre-built components.
                       </p>
                     </div>
                   </div>
@@ -65,8 +67,8 @@ export default function Profile() {
                         <h4 className="text-gradient">CSS3</h4>
                       </div>
                       <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolorem illo blanditiis{" "}
+                        Evolution of CSS allowing for more creative web design
+                        with animations, transitions, and layouts.{" "}
                       </p>
                     </div>
                   </div>
@@ -81,8 +83,8 @@ export default function Profile() {
                         <h4 className="text-gradient">HTML5</h4>
                       </div>
                       <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Dolorem illo blanditiis{" "}
+                        Latest standard for web page structure supporting
+                        stronger semantics with audio, video, and graphics.{" "}
                       </p>
                     </div>
                   </div>
