a4aeb7fcff8248e88aa92049bfab8dee1638dd56
Dwi Nur firmanto <dwinurfirmanto@MacBook-Air-Dwi.local>
2024-08-27 15:43:46 +0700

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Footer() {
-  return <div className="text-center">2023 By Dwi Nur Firmanto</div>
+  return <div className="text-center">©2024 By Dwi Nur Firmanto</div>
 }
